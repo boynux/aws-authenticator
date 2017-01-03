7d4122affafdf253050f391482815956ea3139dd
Quentin Nerden <quentin.nerden@stylight.com>
2017-01-03 11:54:34 +0100

    Add comments, get lint to be green, remove useless code (#4)
    * Add comments
    
    * Extra comments
    
    * Making linter happy
    
    Camel cases for everything (no underscores)
    No const all upercase
    Don't start const name with a capital: no point to exporting them.
    
    * Fix comments
    
    * Remove useless pieces of code
    
    AWSAssumeRoleArn already have the default string value ("")
    Replacing ~/ by the real user's path on the system is already done
    before
    
    * Fix comments

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Package main allows to update the file ~/.aws/credentials with a valid AWS session token.
 package main
 
 import (
@@ -17,36 +18,41 @@ import (
 )
 
 const (
-	GET_TOKEN_PROFILE = "getToken"
-	TEMP_FILE_PATH    = "~/.aws/credentials.tmp"
-	ORIG_FILE_PATH    = "~/.aws/credentials"
-	DEFAULT_PROFILE   = "default"
+	getTokenProfile = "getToken"
+	tempFilePath    = "~/.aws/credentials.tmp"
+	origFilePath    = "~/.aws/credentials"
+	defaultProfile  = "default"
 )
 
+// Config represent the AWS user's connection information
 type Config struct {
-	AWSAccessKeyId     string
+	AWSAccessKeyID     string
 	AWSSecretAccessKey string
 	AWSSessionToken    string
 	AWSAssumeRoleArn   string
 	AWSDefaultProfile  string
 }
 
+// NewConfig returns an empty Config
 func NewConfig() *Config {
 	return &Config{
-		AWSAssumeRoleArn:  "",
-		AWSDefaultProfile: DEFAULT_PROFILE,
+		AWSDefaultProfile: defaultProfile,
 	}
 }
 
+// main is the entry point of the program.
+// It gets the MFA token from the user, get a token for the session and saves it
+// to the credentials file.
 func main() {
 
 	token := getToken("Please enter MFA token: ")
 
 	conf := NewConfig()
+
 	var err error
 
 	session := session.New(&aws.Config{
-		Credentials: credentials.NewSharedCredentials("", GET_TOKEN_PROFILE),
+		Credentials: credentials.NewSharedCredentials("", getTokenProfile),
 	})
 
 	var iam *iam.User
@@ -60,21 +66,18 @@ func main() {
 		log.Fatal("Error while creating session: ", err.Error())
 	}
 
-	temp_path := expandPath(TEMP_FILE_PATH)
-	orig_path := expandPath(ORIG_FILE_PATH)
+	tempPath := expandPath(tempFilePath)
+	origPath := expandPath(origFilePath)
 
-	if temp_path[:2] == "~/" {
-		usr, _ := user.Current()
-		temp_path = usr.HomeDir + temp_path[1:]
-	}
+	c, _ := openConfig(&origPath)
+	writeTempConfig(c, conf, tempPath)
 
-	c, _ := openConfig(&orig_path)
-	writeTempConfig(c, conf, temp_path)
-
-	swapFiles(temp_path, orig_path)
+	swapFiles(tempPath, origPath)
 	log.Println("Credentials has been updated!")
 }
 
+// expandPath replaces ~/' in the path given in parameter
+// by the real path of the user's home directory
 func expandPath(path string) string {
 	if path[:2] == "~/" {
 		usr, _ := user.Current()
@@ -84,10 +87,12 @@ func expandPath(path string) string {
 	return path
 }
 
+//openConfig reads the file in parameter and returns a Config struct
 func openConfig(filename *string) (*config.Config, error) {
 	return config.Read(*filename, "# ", "=", true, true)
 }
 
+// getToken prompts the user for the a token and returns it
 func getToken(prompt string) string {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf(prompt)
@@ -96,6 +101,7 @@ func getToken(prompt string) string {
 	return scanner.Text()
 }
 
+// getCurrentUser returns the IAM user.
 func getCurrentUser(session *session.Session) (*iam.User, error) {
 	svn := iam.New(session)
 	user, err := svn.GetUser(&iam.GetUserInput{})
@@ -103,18 +109,20 @@ func getCurrentUser(session *session.Session) (*iam.User, error) {
 	return user.User, err
 }
 
+// getSessionToken gets a session token for the user. It returns a Config struct that includes it.
 func getSessionToken(
 	session *session.Session,
 	user *iam.User,
-	token string, args ...string) (*Config, error) {
+	token string,
+	args ...string) (*Config, error) {
 
-	assume_role := ""
-	default_profile := DEFAULT_PROFILE
+	assumeRole := ""
+	defaultProfile := defaultProfile
 
 	if len(args) > 0 {
-		assume_role = args[0]
+		assumeRole = args[0]
 		if len(args) > 1 {
-			default_profile = args[1]
+			defaultProfile = args[1]
 		}
 	}
 
@@ -132,22 +140,24 @@ func getSessionToken(
 	config := &Config{
 		AWSSessionToken:    *out.Credentials.SessionToken,
 		AWSSecretAccessKey: *out.Credentials.SecretAccessKey,
-		AWSAccessKeyId:     *out.Credentials.AccessKeyId,
-		AWSAssumeRoleArn:   assume_role,
-		AWSDefaultProfile:  default_profile,
+		AWSAccessKeyID:     *out.Credentials.AccessKeyId,
+		AWSAssumeRoleArn:   assumeRole,
+		AWSDefaultProfile:  defaultProfile,
 	}
 
 	return config, err
 }
 
+// writeTempConfig save the config in parameter to file tmp
 func writeTempConfig(c *config.Config, config *Config, tmp string) error {
 	c.AddSection("default")
-	c.AddOption("default", "aws_access_key_id", config.AWSAccessKeyId)
+	c.AddOption("default", "aws_access_key_id", config.AWSAccessKeyID)
 	c.AddOption("default", "aws_session_token", config.AWSSessionToken)
 	c.AddOption("default", "aws_secret_access_key", config.AWSSecretAccessKey)
 	c.AddOption("default", "role_arn", config.AWSAssumeRoleArn)
 	c.AddOption("default", "source_profile", config.AWSDefaultProfile)
 
+	// role_arn and source_profile only make sense if AWSAssumeRoleArn is set
 	if config.AWSAssumeRoleArn == "" {
 		c.RemoveOption("default", "role_arn")
 		c.RemoveOption("default", "source_profile")
@@ -156,6 +166,7 @@ func writeTempConfig(c *config.Config, config *Config, tmp string) error {
 	return c.WriteFile(tmp, 0644, "Updated by Boynux authenticator")
 }
 
+// swapFiles renames file temp into file orig
 func swapFiles(orig, temp string) error {
 	return os.Rename(orig, temp)
 }
